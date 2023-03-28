bceaf6dda8162527c6732c4a85c9a59b88d4cb0f
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-03-28 20:10:53 +0200

    strategy

--- src/BuyAndHold_Strategy.cpp
+++ src/BuyAndHold_Strategy.cpp
@@ -1,10 +1,20 @@
 #include "BuyAndHold_Strategy.h"
 
-BuyAndHold_Strategy :: BuyAndHold_Strategy() {
+BuyAndHold_Strategy :: BuyAndHold_Strategy(std::string in_bars, std::string in_events) {
+	bars = in_bars;
+	events = in_events;
 
 }
 
 BuyAndHold_Strategy :: ~BuyAndHold_Strategy() {
+
+}
+
+void BuyAndHold_Strategy :: calculate_initial_bought() {
+
+}
+
+void BuyAndHold_Strategy :: calculate_signals() {
 
 }
 
--- src/BuyAndHold_Strategy.h
+++ src/BuyAndHold_Strategy.h
@@ -4,8 +4,15 @@
 
 class BuyAndHold_Strategy : public Strategy_Base {
 	public:
-		BuyAndHold_Strategy();
+		BuyAndHold_Strategy(std::string in_bars, std::string in_events);
 		~BuyAndHold_Strategy();
+		void calculate_initial_bought();
+		void calculate_signals();
+
+		std::string bars;
+		std::string symbol_list;
+		std::string events;
+
 
 	private:
 };
