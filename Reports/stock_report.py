ed1b3d8a4e3dc6f44b9f8a210c3d6b21994ddef3
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-03-28 19:04:06 +0200

    report

--- Reports/stock_report.py
+++ Reports/stock_report.py
@@ -6,23 +6,21 @@ import os
 import sys
 import datetime
 import matplotlib.dates as mdates
+import mplfinance as mpf
 
-sys.path.append(str(sys.path[0][:-14]))
-dirname = os.getcwd()
-dirname = dirname.replace("/Reports", "")
 
-def create_report(csv_path, interval):
-    csv_path = dirname+"/Data/YahooData/data/TSLA_2023-03-25_1m"
+class StockReport():
+    def __init__(self, csv_path):
+        sys.path.append(str(sys.path[0][:-14]))
+        dirname = os.getcwd()
+        dirname = dirname.replace("/Reports", "")
+        self.csv_path = dirname + csv_path
+        self.ticker = csv_path.split("/")[-1].split("_")[0]
+        self.df = pd.read_csv(self.csv_path, index_col=0, parse_dates=True)
 
-    stock = csv_path.split("/")[-1].split("_")[0]
-    df = pd.read_csv(csv_path)
-    df.plot(x="Datetime", y="Close",rot=50)
-    plt.xlabel("y")
-    plt.ylabel("x")
-    plt.title("title")
-    plt.tight_layout()
-
-    plt.show()
-
-create_report("test", "1m")
-
+    def generate_plot(self):
+        mpf.plot(self.df, type="line", axtitle="Closing price of " + self.ticker)
+        
+csv_path = "/Data/YahooData/data/TSLA_2023-03-25_1m"
+Report = StockReport(csv_path)
+Report.generate_plot()
