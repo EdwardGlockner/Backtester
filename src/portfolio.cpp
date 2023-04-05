238429868b4dc07f7812086eed527f3877d1b406
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-04-05 21:38:16 +0200

    done with portfolio

--- src/portfolio.cpp
+++ src/portfolio.cpp
@@ -16,13 +16,13 @@ void Portfolio::update_fill(Event event) {
 
 }
 
-NaivePortfolio::NaivePortfolio(std::vector<HistoricCSVDataHandler::Bar> in_bars, std::vector<Event> in_events, std::string in_start_date, float in_initial_capital) {
+NaivePortfolio::NaivePortfolio(HistoricCSVDataHandler in_bars, std::vector<Event> in_events, std::string in_start_date, float in_initial_capital) {
 
 	bars = in_bars;
 	events = in_events;
 	start_date = in_start_date;
 	initial_capital = in_initial_capital;
-	symbol_list = create_symbol_list();
+	symbol_list = bars.symbol_list;
 	all_positions = construct_all_positions();
 	current_positions = construct_current_positions();
 	all_holdings = construct_all_holdings();
@@ -104,19 +104,78 @@ std::map<std::string, std::string> NaivePortfolio::construct_current_holdings() 
 
 }
 
-void NaivePortfolio::update_time_index() {
+void NaivePortfolio::update_time_index(Event event) {
+	std::map<std::string, HistoricCSVDataHandler::Bar> temp_bars;
+	for (int i = 0; i < symbol_list.size(); i++) {
+		temp_bars[symbol_list[i]] = bars.get_latest_bars(symbol_list[i], 1);
+	}	
+
+	std::map<std::string, std::string> dp;
+	dp = construct_current_positions();	
+	dp["datetime"] = temp_bars[symbol_list[0]].DateTime;
+	
+	for (int i = 0; i < symbol_list.size(); i++) {
+		dp[symbol_list[i]] = current_positions[symbol_list[i]];
+	}
+
+	all_positions.push_back(dp);
+
+	std::map<std::string, std::string> dh;
+	dh = construct_current_positions();
+	dh["datetime"] = temp_bars[symbol_list[0]].DateTime;
+	dh["cash"] = current_holdings["cash"];
+	dh["commision"] = current_holdings["commision"];
+	dh["total"] = current_holdings["total"];
+
+	for (int i = 0; i < symbol_list.size(); i++) {
+		float market_value = std::stof(current_positions[symbol_list[i]]) * temp_bars[0].Close;
+		dh[symbol_list[i]] = market_value;
+		dh["total"] += market_value;
+	}
+	all_holdings.push_back(dh);
 
 }
 
-void NaivePortfolio::update_positions_from_fill() {
-
+void NaivePortfolio::update_positions_from_fill(Event* event) {
+	if (event->type == Event::FILL_EVENT) {
+		FillEvent* fill_evt = static_cast<FillEvent*>(event);
+		int fill_dir = 0;
+		if (fill_evt-> direction == "BUY") {
+			fill_dir = 1;
+		}
+		else if (fill_evt-> direction == "SELL") {
+			fill_dir = -1;
+		}
+		current_positions[fill_evt->symbol] += fill_dir*fill_evt->quantity;
+	}
 }
 
-void NaivePortfolio::update_holdings_from_fill() {
+void NaivePortfolio::update_holdings_from_fill(Event* event) {
+	if (event->type == Event::FILL_EVENT) {
+		FillEvent* fill_evt = static_cast<FillEvent*>(event);
+		int fill_dir = 0;
+		if (fill_evt->direction == "BUY") {
+			fill_dir = 1;
+		}
+		else if (fill_evt->direction == "SELL") {
+			fill_dir = -1;
+		}
+		float fill_cost = bars.get_latest_bars(fill_evt->symbol, 1).Close;
+		float cost = fill_dir * fill_cost * fill_evt->quantity;
 
+		current_holdings[fill_evt->symbol] += cost;
+		current_holdings["commision"] += fill_evt->commision;
+		current_holdings["cash"] = std::stof(current_holdings["cash"]) - (cost + std::stof(fill_evt->commision));
+		current_holdings["total"] = std::stof(current_holdings["total"]) - (cost + std::stof(fill_evt->commision));
+	}
 }
 
-void NaivePortfolio::update_fill() {
+void NaivePortfolio::update_fill(Event* event) {
+	if (event->type == Event::FILL_EVENT) {
+		FillEvent* fill_evt = static_cast<FillEvent*>(event);
+		update_positions_from_fill(fill_evt);
+		update_holdings_from_fill(fill_evt);
+	}
 
 }
 
@@ -167,13 +226,4 @@ void NaivePortfolio::update_signal(Event* event) {
 
 }
 
-std::vector<std::string> NaivePortfolio::create_symbol_list() {
-	std::vector<std::string> list;
-	// Extract symbols from MyStruct vector
-  	for (int i = 0; i < bars.size(); i++) {
-    		list.push_back(bars[i].Symbol);
-  	}
-
-	return list;
-}
 
--- src/portfolio.h
+++ src/portfolio.h
@@ -18,22 +18,21 @@ class Portfolio {
 class NaivePortfolio : public Portfolio{
 	public:
 		HistoricCSVDataHandler data_handler;
-		NaivePortfolio(std::vector<HistoricCSVDataHandler::Bar> in_bars, std::vector<Event> in_events, std::string in_start_date, float in_initial_capital = 1000000);
+		NaivePortfolio(HistoricCSVDataHandler in_bars, std::vector<Event> in_events, std::string in_start_date, float in_initial_capital = 1000000);
 		~NaivePortfolio();
 
 		std::vector<std::map<std::string, std::string> > construct_all_positions();
 		std::map<std::string, std::string> construct_current_positions();
 		std::vector<std::map<std::string, std::string> > construct_all_holdings();
 		std::map<std::string, std::string> construct_current_holdings();
-		void update_time_index();
-		void update_positions_from_fill();
-		void update_holdings_from_fill();
-		void update_fill();
+		void update_time_index(Event event);
+		void update_positions_from_fill(Event* event);
+		void update_holdings_from_fill(Event* event);
+		void update_fill(Event* event);
 		OrderEvent generate_naive_order(Event* event);
 		void update_signal(Event* event);
-		std::vector<std::string> create_symbol_list();
 		
-		std::vector<HistoricCSVDataHandler::Bar> bars;
+		HistoricCSVDataHandler bars;
 	       	std::vector<Event> events;
 		std::string start_date;	
 		std::vector<std::string> symbol_list;
