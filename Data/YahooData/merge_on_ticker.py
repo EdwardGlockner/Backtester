f5b036c89be44c8f5dd62174b1b953da1a372819
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-04-08 12:07:10 +0200

    merge files

--- Data/YahooData/merge_on_ticker.py
+++ Data/YahooData/merge_on_ticker.py
@@ -1,10 +1,61 @@
+#--IMPORTS--------------------+
 import sys
 import os
 import pandas as pd
 import numpy as np
+import glob
 
 #--FIXING PATH----------------+
 sys.path.append(str(sys.path[0][:-14]))
 dirname = os.getcwd()
 
+#--GLOBALS--------------------+
+directory = dirname + "/data/"
 
+#--FUNCTIONS------------------+
+def create_dict():
+    """ 
+    
+    @params:
+        None
+    @returns:
+        tick_map:
+    """
+    
+    tick_map = {}
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+
+        # checking if it is a file
+        if not os.path.isfile(f):
+            break
+
+        ticker = f.split("_")[0].split("/")[-1]
+        if ticker in tick_map:
+            tick_map[ticker].append(f)
+        else:
+            tick_map[ticker] = [f]
+
+    return tick_map
+
+
+def merge_files(tick_map):
+    #for tick in tick_map:
+    #    combined_csv = pd.concat([pd.read_csv(f) for f in tick_map[tick]])
+    #    print(f"Done merging {tick}") 
+    combined_csv = pd.concat(pd.read_csv(f) for f in tick_map["TSLA"])
+
+    print(combined_csv)
+
+
+def cleaning_csv(csv):
+    pass
+
+#--MAIN----------------------+
+def main():
+    tick_map = create_dict()
+    merge_files(tick_map)
+    
+#--RUN CODE------------------+
+if __name__ == "__main__":
+    main()
